9b3a94aee330348ad18bbc4831f002b62f8700f2
Evander_Jose <jemandexistiert@gmail.com>
2022-07-16 16:09:03 +0700

    Prototype Level, Level_0, done. Well, this doesn't incorporate the main dice block mechanic yet, but it does show that the basics are there: moving, jumping, dying and moving on to the next level.

--- GMTK-godot/Scripts/Level Loading/sceneLoader.gd
+++ GMTK-godot/Scripts/Level Loading/sceneLoader.gd
@@ -4,6 +4,8 @@ var current_level = 0
 var load_level_function
 var load_next_level_function
 
+var busy_loading = false
+
 func _ready():
 	load_level_function = funcref(self, "load_level")
 	load_next_level_function = funcref(self, "load_next_level")
@@ -15,12 +17,12 @@ func load_level(level_number):
 		get_child(0).queue_free()
 		
 	var level_name = "Level_" + str(level_number) + ".tscn"
-	var level_n = load("res://scenes/" + level_name).instance()
+	var level_n = load("res://Levels/" + level_name).instance()
 	add_child(level_n)
 
 
 func load_next_level():
-	var path_to_next_level_file = "res://scenes/Level_" + str(current_level+1) + ".tscn" 
+	var path_to_next_level_file = "res://Levels/Level_" + str(current_level+1) + ".tscn" 
 	if(does_file_exist(path_to_next_level_file)):
 		current_level += 1
 		load_level(current_level)
--- GMTK-godot/Scripts/playerMovement.gd
+++ GMTK-godot/Scripts/playerMovement.gd
@@ -7,10 +7,7 @@ export var speed: = Vector2(300.0, 1000.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# If you can read this, then the I have succesfully pushed this script
-	# to your GitHub repo!
-	# Btw, you use GDScript or C#?
-	pass
+	velocity = Vector2.ZERO
 
 
 func _physics_process(delta):
