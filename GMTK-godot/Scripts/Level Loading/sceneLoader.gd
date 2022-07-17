0b90dca90ffc4c26329e0a2223a3d977f5f44cdf
milksake <caceresmariana443@gmail.com>
2022-07-16 22:11:49 -0500

    added screen shake

--- GMTK-godot/Scripts/Level Loading/sceneLoader.gd
+++ GMTK-godot/Scripts/Level Loading/sceneLoader.gd
@@ -3,18 +3,26 @@ extends Node
 var current_level = 0 
 var load_level_function
 var load_next_level_function
+export var screen_shake := 0
+onready var camera: Camera2D
 
 var busy_loading = false
 
 func _ready():
+	camera = Camera2D.new()
+	camera.current = true
+	camera.anchor_mode = 0
+	add_child(camera)
 	load_level_function = funcref(self, "load_level")
 	load_next_level_function = funcref(self, "load_next_level")
 	load_level_function.call_func(current_level)
 
+func _process(delta):
+	camera.set_offset(Vector2(rand_range(-screen_shake, screen_shake), rand_range(-screen_shake, screen_shake)))
 
 func load_level(level_number):
-	if(get_child_count() > 0):
-		get_child(0).queue_free()
+	if(get_child_count() > 1):
+		get_child(1).queue_free()
 		
 	var level_name = "Level_" + str(level_number) + ".tscn"
 	var level_n = load("res://Levels/" + level_name).instance()
